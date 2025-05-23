f0fc8c4bb391afed00fdd67efbe920e6de4acba2
SharonNaemi <naemi_17@live.de>
2025-05-24 01:31:00 +0200

    cleanup constants

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -30,14 +30,3 @@ export const ArithmeticOperatorNode: string = "arithmeticOperatorNode";
 export const classicalHandle: string = "classicalHandle";
 export const quantumHandle: string = "quantumHandle";
 export const ancillaHandle: string = "ancillaHandle";
-
-export const HadamardImplementation: string = "OPENQASM 3.0;\ninclude \"stdgates.inc\";\n@leqo.input 0\nqubit[1] q;\nh q[0];\n@leqo.output 0\nlet output = q;";
-export const PauliXImplementation: string = "OPENQASM 3.0;\ninclude \"stdgates.inc\";\n@leqo.input 0\nqubit[1] q;\nx q[0];\n@leqo.output 0\nlet output = q;";
-export const PauliYImplementation: string = "OPENQASM 3.0;\ninclude \"stdgates.inc\";\n@leqo.input 0\nqubit[1] q;\ny q[0];\n@leqo.output 0\nlet output = q;";
-export const PauliZImplementation: string = "OPENQASM 3.0;\ninclude \"stdgates.inc\";\n@leqo.input 0\nqubit[1] q;\nz q[0];\n@leqo.output 0\nlet output = q;";
-export const ToffoliImplementation: string = "OPENQASM 3.0;\ninclude \"stdgates.inc\";\n@leqo.input 0\nqubit[1] q0;\n@leqo.output 1\nqubit[1] q1;\n@leqo.input 2\nqubit[1] q2;\nccx q0, q1, q2;\n@leqo.output 0\nlet output0 = q0;\n@leqo.output 1\nlet output1 = q1;\n@leqo.output 2\nlet output2 = q2;";
-export const CnotImplementation: string = "OPENQASM 3.0;\ninclude \"stdgates.inc\";\n@leqo.input 0\nqubit[1] q0;\n@leqo.input 0\nqubit[1] q1;\ncx q0, q1;\n@leqo.output 0\nlet output0 = q0;\n@leqo.output 1\nlet output1 = q1;";
-
-export const RXImplementation: string = "OPENQASM 3.0;\ninclude \"stdgates.inc\";\ninput float[64] theta;\n@leqo.input 0\nqubit[1] q0;\nrx(theta) q0[0];\n@leqo.output 0\nlet output0 = q0;"
-export const RYImplementation: string = "OPENQASM 3.0;\ninclude \"stdgates.inc\";\ninput float[64] theta;\n@leqo.input 0\nqubit[1] q0;\nry(theta) q0[0];\n@leqo.output 0\nlet output0 = q0;"
-export const RZImplementation: string = "OPENQASM 3.0;\ninclude \"stdgates.inc\";\ninput float[64] theta;\n@leqo.input 0\nqubit[1] q0;\nrz(theta) q0[0];\n@leqo.output 0\nlet output0 = q0;"
