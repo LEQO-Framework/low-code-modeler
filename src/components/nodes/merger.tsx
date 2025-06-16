1fe39c58ae401cf5cdbbeb7874d146a4ab0d14fe
SharonNaemi <naemi_17@live.de>
2025-06-17 00:40:34 +0200

    add uniform superposition use cases, fix merger

--- src/components/nodes/merger.tsx
+++ src/components/nodes/merger.tsx
@@ -24,10 +24,10 @@ export const MergerNode = memo((node: Node) => {
   let { edges, setEdges, setNewEdges } = useStore(selector, shallow);
   const [edgesGraph, setEdgesGraph] = useState(edges);
 
-  const numberInputs = Math.max(data.numberInputs,1);
+  const numberInputs = data.numberInputs||1;
   const numberOutputs = data.numberOutputs || 1;
 
-  const handleCount = Math.max(numberInputs, numberOutputs, 1);
+  const handleCount = Math.max(numberInputs, 1);
   console.log(handleCount)
 
   const handleGap = 40;
