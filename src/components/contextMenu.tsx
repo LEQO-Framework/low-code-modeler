08c5ddf0e7316e5f72c72ebb183616c808fab73d
Alina <st177132@stud.uni-stuttgart.de>
2025-10-26 21:38:54 +0100

    Context Menu: "Add/Remove" buttons
    Added buttons to add and remove input/output for some nodes, wherever applicable.
    
    - if-then-else & repeat: add/remove quantum/classical input
    - merger: add/remove input
    - splitter: add/remove output

--- src/components/contextMenu.tsx
+++ src/components/contextMenu.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useReactFlow, Node } from "reactflow";
-import { FaTrash, FaCopy } from "react-icons/fa";
+import { FaTrash, FaCopy, FaPlus, FaMinus } from "react-icons/fa";
 import { v4 as uuid } from "uuid";
+import { Button } from "./ui";
 
 interface ContextMenuProps {
   id: string;
@@ -27,6 +28,8 @@ export const ContextMenu: React.FC<ContextMenuProps> = ({
   styles = {}, // Default styles as an empty object
 }) => {
   const { getNode, setNodes, addNodes, setEdges } = useReactFlow();
+  const type = getNode(id).type
+  console.log("type", type)
 
   const duplicateNode = useCallback(() => { // duplicate node at slighlty shifted position
     const node = getNode(id);
@@ -54,46 +57,144 @@ export const ContextMenu: React.FC<ContextMenuProps> = ({
     if (onAction) onAction("delete", id);
   }, [id, setNodes, setEdges, onAction]);
 
-  return (
-    <div
-      style={{
-        position: "absolute",
-        top,
-        left,
-        ...styles,
-        backgroundColor: "#fff",
-        border: "1px solid #ddd",
-        borderRadius: "8px",
-        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-        padding: "10px",
-        zIndex: 1000,
-        display: "flex",
-        flexDirection: "column",
-        gap: "10px",
-      }}
-      className="context-menu"
-    >
-      <p style={{ margin: "0.5em", fontSize: "14px", color: "#666" }}>
-        <small>Node: {id}</small>
-      </p>
-      <div>
-        <button
-          onClick={duplicateNode}
-          style={buttonStyle}
-          aria-label="Duplicate Node"
-        >
-          <FaCopy style={iconStyle} /> Duplicate
-        </button>
-        <button
-          onClick={deleteNode}
-          style={buttonStyle}
-          aria-label="Delete Node"
-        >
-          <FaTrash style={iconStyle} /> Delete
-        </button>
+  const incrementNodeDataField = useCallback((field: string, min_value: number) => { //TODO: richtiges Feld verwenden (bei merger: numberInputs) --> Feld als Argument übergeben?
+    const node = getNode(id);
+    //const field = "numberQuantumInputs";
+    setNodes((nodes) =>
+      nodes.map((n) =>
+        n.id === id
+          ? { ...n, data: { ...n.data, [field]: n.data[field]? n.data[field] + 1 : min_value+1 } }
+          : n
+      )
+    );
+  }, [id, getNode, setNodes, onAction]);
+
+  const decrementNodeDataField = useCallback((field: string, min_value: number) => {
+    const node = getNode(id);
+    setNodes((nodes) =>
+      nodes.map((n) =>
+        n.id === id
+          ? { ...n, data: { ...n.data, [field]: n.data[field]>min_value? n.data[field] - 1 : min_value } }
+          : n
+      )
+    );
+  }, [id, getNode, setNodes, onAction]);
+  const [submenuOpen, setSubmenuOpen] = useState(false);
+  let buttons = [];
+  if (type == "ifElseNode" || type == "controlStructureNode"){
+    buttons.push(
+      <button
+        onClick={() => incrementNodeDataField("numberQuantumInputs",1)}
+        style={buttonStyle}
+        aria-label="Add Quantum Input"
+      >
+        <FaPlus style={iconStyle} /> Add Quantum Input
+      </button>);
+    buttons.push(
+      <button
+        onClick={() => incrementNodeDataField("numberClassicalInputs",1)}
+        style={buttonStyle}
+        aria-label="Add Classical Input"
+      >
+        <FaPlus style={iconStyle} /> Add Classical Input
+      </button>);
+    buttons.push(
+      <button
+        onClick={() => decrementNodeDataField("numberQuantumInputs",1)} // Placeholder
+        style={buttonStyle}
+        aria-label="Remove Quantum Input"
+      >
+        <FaMinus style={iconStyle} /> Remove Quantum Input
+      </button>);
+    buttons.push(
+      <button
+        onClick={() => decrementNodeDataField("numberClassicalInputs",1)} // Placeholder
+        style={buttonStyle}
+        aria-label="Remove Classical Input"
+      >
+        <FaMinus style={iconStyle} /> Remove Classical Input
+      </button>);
+  } else if (type == "mergerNode"){
+    buttons.push(
+      <button
+        onClick={() => incrementNodeDataField("numberInputs",2)}
+        style={buttonStyle}
+        aria-label="Add Input"
+      >
+        <FaPlus style={iconStyle} /> Add Input
+      </button>);
+    buttons.push(
+      <button
+        onClick={() => decrementNodeDataField("numberInputs",2)} // Placeholder
+        style={buttonStyle}
+        aria-label="Remove Input"
+      >
+        <FaMinus style={iconStyle} /> Remove Input
+      </button>);
+  } else if(type == "splitterNode"){
+        buttons.push(
+      <button
+        onClick={() => incrementNodeDataField("numberOutputs",2)}
+        style={buttonStyle}
+        aria-label="Add Output"
+      >
+        <FaPlus style={iconStyle} /> Add Output
+      </button>);
+    buttons.push(
+      <button
+        onClick={() => decrementNodeDataField("numberOutputs",2)} // Placeholder
+        style={buttonStyle}
+        aria-label="Remove Output"
+      >
+        <FaMinus style={iconStyle} /> Remove Output
+      </button>);
+  } 
+  // every node can be duplicated and deleted
+  buttons.push(     
+    <button
+      onClick={duplicateNode}
+      style={buttonStyle}
+      aria-label="Duplicate Node"
+      >
+        <FaCopy style={iconStyle} /> Duplicate
+    </button>);
+  buttons.push(
+    <button
+      onClick={deleteNode}
+      style={buttonStyle}
+      aria-label="Delete Node"
+      >
+        <FaTrash style={iconStyle} /> Delete
+      </button>);
+  console.log("buttons", buttons)
+  // find better solution for following (too much duplicated code)
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top,
+          left,
+          ...styles, // Merge passed styles
+          backgroundColor: "#fff",
+          border: "1px solid #ddd",
+          borderRadius: "8px",
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+          padding: "10px",
+          zIndex: 1000,
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+        }}
+        className="context-menu"
+      >
+        <p style={{ margin: "0.5em", fontSize: "14px", color: "#666" }}>
+          <small>Node: {id}</small>
+        </p>
+        <div>
+          {buttons}
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 const buttonStyle: React.CSSProperties = {
