65764029cc9324c103b84737f24019a704470331
Alina <st177132@stud.uni-stuttgart.de>
2026-01-29 21:13:52 +0100

    add dropdown menu logic
    * dropdown menu for toolbar
    * uses @radix-ui/react-dropdown-menu

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { Button } from "./button/button";
 export { useToast } from "./toast/use-toast";
 export { Toaster } from "./toast/toaster";
+export { DropdownMenu, DropdownMenuTrigger,DropdownMenuContent,DropdownMenuItem } from "./dropdownMenu/dropdownMenu";
