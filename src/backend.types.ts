13fb58298f080ec4f5d8cdf13c122aeaab488e14
MaxSilvester <138241982+MaxSilvesterQWE@users.noreply.github.com>
2026-02-06 16:54:10 +0100

    backend-types: add FileLiteralNode and StringLiteralNode

--- src/backend.types.ts
+++ src/backend.types.ts
@@ -183,7 +183,7 @@ export interface components {
         CompileRequest: {
             metadata: components["schemas"]["MetaData"];
             /** Nodes */
-            nodes: (components["schemas"]["ImplementationNode"] | components["schemas"]["ArrayLiteralNode"] | components["schemas"]["QubitNode"] | components["schemas"]["EncodeValueNode"] | components["schemas"]["PrepareStateNode"] | components["schemas"]["SplitterNode"] | components["schemas"]["MergerNode"] | components["schemas"]["MeasurementNode"] | components["schemas"]["GateNode"] | components["schemas"]["ParameterizedGateNode"] | components["schemas"]["BitLiteralNode"] | components["schemas"]["BoolLiteralNode"] | components["schemas"]["IntLiteralNode"] | components["schemas"]["FloatLiteralNode"] | components["schemas"]["AncillaNode"] | components["schemas"]["IfThenElseNode"] | components["schemas"]["RepeatNode"] | components["schemas"]["OperatorNode"] | components["schemas"]["PluginNode"])[];
+            nodes: (components["schemas"]["ImplementationNode"] | components["schemas"]["ArrayLiteralNode"] | components["schemas"]["QubitNode"] | components["schemas"]["EncodeValueNode"] | components["schemas"]["PrepareStateNode"] | components["schemas"]["SplitterNode"] | components["schemas"]["MergerNode"] | components["schemas"]["MeasurementNode"] | components["schemas"]["GateNode"] | components["schemas"]["ParameterizedGateNode"] | components["schemas"]["BitLiteralNode"] | components["schemas"]["BoolLiteralNode"] | components["schemas"]["IntLiteralNode"] | components["schemas"]["FloatLiteralNode"] | components["schemas"]["FileLiteralNode"] | components["schemas"]["StringLiteralNode"] | components["schemas"]["AncillaNode"] | components["schemas"]["IfThenElseNode"] | components["schemas"]["RepeatNode"] | components["schemas"]["OperatorNode"] | components["schemas"]["PluginNode"])[];
             /** Edges */
             edges: components["schemas"]["Edge"][];
             compilation_target: string;
@@ -226,6 +226,22 @@ export interface components {
              * @default 0
              */
             bounds?: number;
+        };
+        /** FileLiteralNode */
+        FileLiteralNode: {
+            /** Id */
+            id: string;
+            /** Label */
+            label?: string | null;
+            /**
+             * @description discriminator enum property added by openapi-typescript
+             * @enum {string}
+             */
+            type: "file";
+            /**
+             * Value
+             */
+            value: string;
         };
         /** FloatLiteralNode */
         FloatLiteralNode: {
@@ -506,6 +522,22 @@ export interface components {
             type: "splitter";
             /** Numberoutputs */
             numberOutputs: number;
+        };
+        /** StringLiteralNode */
+        StringLiteralNode: {
+            /** Id */
+            id: string;
+            /** Label */
+            label?: string | null;
+            /**
+             * @description discriminator enum property added by openapi-typescript
+             * @enum {string}
+             */
+            type: "string";
+            /**
+             * Value
+             */
+            value: string;
         };
         /**
          * StatusBody
