40e905bedf00db53c5813d831e41d26380a37775
anila-m <st177132@stud.uni-stuttgart.de>
2026-02-10 23:01:15 +0100

    create editableDataTypeNode
    * Node for editableDataType (no input Handles, but inputPorts to enter values of properties)
    * inputPort: similar to outputPort but without size and identifier, instead input field for property value

---
+++ src/components/nodes/editableDataType.tsx
@@ -0,0 +1,208 @@
+import { memo, useEffect, useState } from "react";
+import { Handle, Position, Node, useUpdateNodeInternals } from "reactflow";
+import { useStore } from "@/config/store";
+import { shallow } from "zustand/shallow";
+import OutputPort from "../utils/outputPort";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import { classicalConstructColor } from "@/constants";
+import { findDuplicateOutputIdentifier, findDuplicateOutputIdentifiersInsideNode } from "../utils/utils";
+import { AlertCircle } from "lucide-react";
+import InputPort from "../utils/inputPort";
+
+const selector = (state: any) => ({
+  selectedNode: state.selectedNode,
+  nodes: state.nodes,
+  edges: state.edges,
+  ancillaMode: state.ancillaMode,
+  updateNodeValue: state.updateNodeValue,
+  setSelectedNode: state.setSelectedNode,
+  setEdges: state.setEdges,
+  setNewEdges: state.setNewEdges
+});
+
+export const EditableDataTypeNode = memo((node: Node) => {
+  const { data, selected } = node;
+
+  const { edges, nodes, updateNodeValue, setSelectedNode, setNewEdges, ancillaMode } = useStore(selector, shallow);
+  const updateNodeInternals = useUpdateNodeInternals();
+  const properties = data.properties;
+  const constraints = data.constraints;
+  const outputType = data.outputType;
+  const numberInputs = properties.length;
+  const numberOutputs = 1;
+  const label = data.label || "Editable Node";
+
+  const [outputs, setOutputs] = useState(data.outputs || []);
+  const [sizeErrors, setSizeErrors] = useState<{ [key: number]: boolean }>({});
+  const [outputIdentifierErrors, setOutputIdentifierErrors] = useState<{ [key: number]: boolean }>({});
+
+  const headerHeight = 52;
+  const inputHeight = 30;
+  const inputsTotalHeight = numberInputs * inputHeight;
+  const outputHeight = 120;
+  const outputsStartTop = headerHeight + inputsTotalHeight + 10;
+
+
+  useEffect(() => {
+    updateNodeInternals(node.id);
+    setSelectedNode(node);
+    console.log("update editable Node handles")
+  }, [ancillaMode]);
+
+
+  const getInputType = (inputIndex: number) => {
+    const prop = properties[inputIndex];
+    return prop.type
+  };
+
+
+  useEffect(() => {
+    //const selectedNode = nodes.find(n => n.id === node.id);
+    let selectedNode = node;
+    const newErrors: { [key: number]: boolean } = {};
+    const newSizeErrors: { [key: number]: boolean } = {};
+
+    outputs.forEach((output: any, index: number) => {
+      const outputIdentifier = output?.identifier?.trim();
+      const size = output?.size?.trim();
+      if (!outputIdentifier) return;
+
+      const duplicates = findDuplicateOutputIdentifier(nodes, selectedNode.id, selectedNode, outputIdentifier);
+      let isDuplicate = false;
+      for (const [key] of duplicates.entries()) {
+        if (Array.isArray(key) && key.some((item: any) => item.identifier === outputIdentifier)) {
+          isDuplicate = true;
+          break;
+        }
+      }
+      isDuplicate = isDuplicate || findDuplicateOutputIdentifiersInsideNode(nodes, selectedNode, outputIdentifier);
+      const startsWithDigit = /^\d/.test(outputIdentifier);
+
+      newErrors[index] = startsWithDigit || isDuplicate;
+
+      if (size) {
+        newSizeErrors[index] = !/^\d/.test(size);
+      }
+    });
+
+    setOutputIdentifierErrors(newErrors);
+    setSizeErrors(newSizeErrors);
+  }, [nodes, outputs, node.id]);
+
+  const baseHeight = !ancillaMode ? 50 : 200;
+  const extraHeightPerVariable = 130;
+  const dynamicHeight = baseHeight + 2 * 40 + 3 * 50 + numberInputs * 50 + numberOutputs * extraHeightPerVariable;
+
+  return (
+    <motion.div
+      className="relative"
+      initial={false}
+      animate={{
+        width: 320,
+        height: dynamicHeight,
+      }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+    >
+      <div
+        className={cn(
+          "w-[320px] bg-white border border-solid border-gray-700 shadow-md relative",
+          selected && "border-blue-500"
+        )}
+        style={{
+          height: `${dynamicHeight}px`,
+          borderRadius: "28px",
+        }}
+      >
+
+        {(Object.values(outputIdentifierErrors).some(Boolean) ||
+          Object.values(sizeErrors).some(Boolean)) && (
+            <div className="absolute top-2 right-[-40px] group z-20">
+              <AlertCircle className="text-red-600 w-5 h-5" />
+              <div className="absolute top-5 left-[20px] z-10 bg-white text-xs text-red-600 border border-red-400 px-3 py-1 rounded shadow min-w-[150px] whitespace-nowrap">
+                One or more outputs have errors (duplicate ID or size is not a number).
+              </div>
+            </div>
+          )}
+
+        <div
+          className="relative w-full h-[52px] overflow-hidden rounded-t-[28px]"
+        >
+          <div className="w-full h-full bg-orange-300 flex items-center py-1 px-2">
+            <img
+              src="classicalAlgorithmIcon.png" //TODO?
+              alt="icon"
+              className="w-[42px] h-[42px] object-contain flex-shrink-0"
+            />
+            <div className="h-full w-[1px] bg-black mx-2" />
+            
+              <span
+                className="font-semibold leading-none cursor-pointer"
+                style={{ paddingLeft: "25px" }}
+              >
+                {label}
+              </span>
+          </div>
+        </div>
+
+        <div className="mt-[5px] flex flex-col relative z-0">
+          {properties.map((prop, index) => (
+            <div
+            key={`classical-input-${index}`}
+            className="absolute left-0 w-full z-10"
+            //style={{ top: `${outputsStartTop + index * outputHeight}px` }}
+          >
+            <InputPort
+                key={`classicalHandleOperationInput${index}${node.id}`} //TODO: anderer key?
+                node={node}
+                index={index}
+                type={"classical"}
+                nodes={nodes}
+                edges={edges}
+                inputIdentifierError={outputIdentifierErrors[index]}
+                updateNodeValue={updateNodeValue}
+                setInputIdentifierError={(error) => setOutputIdentifierErrors(prev => ({ ...prev, [index]: error }))}
+                setSelectedNode={setSelectedNode}
+                active={true} 
+                propertyName={prop.name} 
+                propertyType={prop.type}            
+            />
+          </div>
+          ))}
+        </div>
+        <div className="custom-node-port-out">
+        {Array.from({ length: numberOutputs }).map((_, index) => (
+          <div
+            key={`output-wrapper-${index}`}
+            className="absolute left-0 w-full z-10"
+            style={{ top: `${outputsStartTop + index * outputHeight}px` }}
+          >
+            <OutputPort
+              key={`output-port-${index}`}
+              node={node}
+              index={index}
+              type={"classical"}
+              nodes={nodes}
+              outputs={outputs}
+              setOutputs={setOutputs}
+              edges={edges}
+              outputIdentifierError={outputIdentifierErrors[index]}
+              updateNodeValue={updateNodeValue}
+              setOutputIdentifierError={(error) =>
+                setOutputIdentifierErrors(prev => ({ ...prev, [index]: error }))
+              }
+              sizeError={sizeErrors[index]}
+              setSizeError={(error) =>
+                setSizeErrors((prev) => ({ ...prev, [index]: error }))
+              }
+              setSelectedNode={setSelectedNode}
+              active={true}
+            />
+          </div>
+        ))}
+        </div>
+
+      </div>
+    </motion.div>
+  );
+});
---
+++ src/components/utils/inputPort.tsx
@@ -0,0 +1,256 @@
+import { Handle, Position } from 'reactflow';
+import { cn } from "@/lib/utils";
+import { findDuplicateOutputIdentifiersInsideNode, isUniqueIdentifier } from "../utils/utils";
+import { Node } from "reactflow";
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { AlgorithmNode, ClassicalAlgorithmNode, classicalConstructColor } from '@/constants';
+
+
+
+interface inputPortProps {
+  node: Node;
+  index?: number;
+  type: string;
+  nodes: Node[];
+  inputs?: any[];
+  setInputs?: (inputs: any[]) => void;
+  propertyName: string;
+  propertyType: string;
+  edges: any[];
+  inputIdentifierError: boolean;
+  updateNodeValue: (nodeId: string, field: string, value: any) => void;
+  setSelectedNode: (node: Node) => void;
+  setInputIdentifierError: (error: boolean) => void;
+  active: boolean;
+}
+
+export default function InputPort({
+  node,
+  index = 0,
+  type,
+  nodes,
+  inputs,
+  setInputs,
+  edges,
+  inputIdentifierError,
+  updateNodeValue,
+  setSelectedNode,
+  setInputIdentifierError,
+  active,
+  propertyName,
+  propertyType,
+}: inputPortProps) {
+  const isClassical = type === "classical";
+  const isAncilla = type === "ancilla";
+  const handleId = isClassical
+    ? `classicalHandle${node.type}Input${index}${node.id}`
+    : isAncilla
+      ? `ancillaHandleInput${index}${node.id}`
+      : `quantumHandle${node.type}Input${index}${node.id}`;
+
+  const handleClass = isClassical
+    ? "classical-circle-port-out"
+    : "circle-port-out";
+
+  const wasBellState = useRef(false);
+  const inputIdentifier = propertyName;
+  const isConnected = edges.some(edge => edge.sourceHandle === handleId);
+  const [propertyValue, setPropertyValue] = useState(""); // TODO: sollte übergeben werden, wie inputs/outputs
+
+
+/*   // Get input type for classical arithmetic operator
+  const getInputType = (inputIndex: number) => {
+    const handleId = `classicalHandleOperationInput${inputIndex}${node.id}`;
+    const edge = edges.find(e => e.target === node.id && e.targetHandle === handleId);
+    if (!edge) return "any";
+
+    const sourceNode = nodes.find(n => n.id === edge.source);
+    if (!sourceNode) return "any";
+
+    if (sourceNode.type === "measurementNode") return "array";
+    if (sourceNode.data?.inputs?.[0]?.value !== undefined) return typeof sourceNode.data.inputs[0].value;
+    if (sourceNode.type === "dataTypeNode") return sourceNode.data?.dataType ?? "any";
+
+    return "any";
+  }; */
+/* 
+  // Determine displayed input type dynamically
+  const displayedInputType = useMemo(() => {
+    let inputType = "unknown";
+
+    // Classical Data Type
+    if (type === "classical" && node.type === "dataTypeNode") {
+      inputType = node.data?.dataType ?? "array";
+    }
+    // Classical Arithmetic Operator: infer from inputs
+    else if (type === "classical" && node.data.label?.includes("Arithmetic Operator")) {
+      const t0 = getInputType(0);
+      const t1 = getInputType(1);
+
+      if (t0 !== "any" && t1 !== "any" && t0 === t1) inputType = t0;
+      else if (t0 !== "any") inputType = t0;
+      else if (t1 !== "any") inputType = t1;
+      else inputType = "any";
+    }
+    // Other classical operators
+    else if (type === "classical" && node.data.label?.includes("Bitwise Operator")) {
+      inputType = "bit";
+    }
+    else if (node.data.label?.includes("Comparison Operator")) {
+      inputType = "boolean";
+    }
+    else if (node.data.label?.includes("Min & Max Operator")) {
+      inputType = "number";
+    }
+    // Quantum or fallback
+    else if (type === "quantum" || type === "ancilla") {
+      inputType = "quantum register";
+    }
+    else if (type === "classical" && (node.type === "algorithmNode" || node.type === "classicalAlgorithmNode")) {
+      inputType = "any"; // default value
+
+      if (node?.data?.inputTypes && node.data.inputTypes[index] != null) {
+        inputType = node.data.inputTypes[index];
+      }
+
+    }
+    else if (type === "classical") {
+      inputType = "array";
+    }
+
+
+    console.log("INPUT PORT displayedInputType", node.id, inputType)
+
+    return inputType;
+  }, [node.id, node.data.label, type, edges, nodes]);
+ */
+  // update inputType if it changes
+/*   useEffect(() => {
+    if (node.data.inputTypes[index] !== displayedInputType) {
+      const updatedInputTypes = { ...node.data.inputTypes }
+      updatedInputTypes[index] = displayedInputType
+      updateNodeValue(node.id, "inputTypes", updatedInputTypes);
+      console.log("Updated node inputType in store:", node.id, displayedInputType);
+    }
+  }, [displayedInputType, node.id, node.data.inputTypes, updateNodeValue]); */
+
+/*   useEffect(() => {
+    const isNowBellState = node.data.quantumStateName?.includes("Bell State");
+    const updatedInputs = [...inputs];
+    if (!updatedInputs[index]) updatedInputs[index] = { identifier: "", size: "" };
+
+    if (isNowBellState) {
+      updatedInputs[index] = { ...updatedInputs[index], size: "2" };
+      setInputs(updatedInputs);
+      node.data.inputs = updatedInputs;
+      updateNodeValue(node.id, "size", "2");
+      updateNodeValue(node.id, "inputs", updatedInputs);
+      setSelectedNode(node);
+      wasBellState.current = true;
+    } else if (wasBellState.current && node.data.label === "Prepare State") {
+      updatedInputs[index] = { ...updatedInputs[index], size: "" };
+      setInputs(updatedInputs);
+      node.data.inputs = updatedInputs;
+      updateNodeValue(node.id, "size", "");
+      updateNodeValue(node.id, "inputs", updatedInputs);
+      setSelectedNode(node);
+      wasBellState.current = false;
+    }
+  }, [node.data.quantumStateName]); */
+
+  return (
+    <div className="relative flex items-center justify-start space-x-0 mt-1">
+      <div
+        className="flex flex-col items-start space-y-1 relative p-2"
+        style={{
+          backgroundColor: isClassical
+            ? classicalConstructColor
+            : isAncilla
+              ? 'rgba(137, 218, 131, 0.2)'
+              : 'rgba(105, 145, 210, 0.2)',
+          width: "120px",
+          borderRadius: isClassical ? '16px' : '0px',
+        }}
+      >
+        <div className="w-full flex justify-between items-center">
+          <span className="text-left text-sm text-black font-semibold">{propertyName}</span>
+          <span className="text-[10px] text-gray-600">
+            type: {propertyType}
+          </span>
+        </div>
+
+        <div className="flex items-center justify-between w-full space-x-2">
+          <input
+            type="text"
+            className={`p-1 text-sm text-black opacity-75 w-20 text-center rounded-full border ${inputIdentifierError
+              ? 'bg-red-500 border-red-500'
+              : isClassical
+                ? 'bg-white border-orange-500'
+                : isAncilla
+                  ? 'bg-white border-green-500'
+                  : 'bg-white border-blue-500'
+              }`}
+            value={propertyValue}
+            onChange={(e) => {let value = e.target.value.trim(); setPropertyValue(value);}}
+          />
+        </div>
+      </div>
+
+      {node.type !== "editableDataTypeNode" && (
+        <Handle
+          type="source"
+          id={handleId}
+          position={Position.Left}
+          className={cn(
+            "z-10",
+            handleClass,
+            "top-1/2 -translate-y-1/2",
+            isClassical
+              ? "!bg-orange-300 !border-black"
+              : isAncilla
+                ? "!bg-green-100 !border-black rotate-45"
+                : "!bg-blue-300 !border-black",
+            (active || isConnected)
+              ? `border-solid ${isClassical
+                ? "!bg-orange-300 !border-black"
+                : isAncilla
+                  ? "!bg-green-100 !border-black rotate-45"
+                  : "!bg-blue-300 !border-black"}`
+              : "!bg-gray-200 !border-dashed !border-gray-500"
+          )}
+          isConnectable={edges.filter(edge => edge.sourceHandle === handleId).length < 1}
+          isConnectableEnd={false}
+        />
+      )}
+    </div>
+  );
+}
+
+export function handleInputIdentifierChange({
+  e,
+  node,
+  nodes,
+  updateNodeValue,
+  setSelectedNode,
+  setInputIdentifierError,
+}: {
+  e: React.ChangeEvent<HTMLInputElement>;
+  node: Node;
+  nodes: Node[];
+  updateNodeValue: (nodeId: string, field: string, value: string) => void;
+  setSelectedNode: (node: Node) => void;
+  setInputIdentifierError: (error: boolean) => void;
+}) {
+  const value = e.target.value;
+  node.data["inputIdentifier"] = value;
+  updateNodeValue(node.id, "inputIdentifier", value);
+  const selectedNode = nodes.find(n => n.id === node.id);
+
+  if (/^\d/.test(value) || (!isUniqueIdentifier(nodes, value, node.id) || findDuplicateOutputIdentifiersInsideNode(nodes, selectedNode, value))) {
+    setInputIdentifierError(true);
+  } else {
+    setInputIdentifierError(false);
+  }
+
+  setSelectedNode(node);
+}
